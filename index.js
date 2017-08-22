31b873fc8a0c8ac4de97c3fc012ce1b6ad4bb6d1
Arcanae <justin.courdesse@gmail.com>
2017-08-22 10:16:09 +0200

    1st commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function fetch(url, obj) {
+    return new Promise((resolve, reject) => {
+        xhr = new XMLHttpRequest();
+        xhr.open(obj.method, url, true);
+        xhr.onload = function() {
+            if (xhr.status = 200) {
+                resolve(xhr.response);
+            } else {
+                reject(xhr.status);
+            }
+        }
+        xhr.onerror = function(error) {
+            reject(error);
+        }
+        xhr.send()
+    });
+}
